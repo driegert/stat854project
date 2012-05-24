09568f502e1b626375687d6e0078637814a70626
Aaron <9ads5@queensu.ca>
2012-05-24 16:38:11 -0400

    The Q7 code is working (I think!).

--- Q7.R
+++ Q7.R
@@ -4,13 +4,26 @@
 Q7 <- function(data, R, AR_order){
   N <- length(data)
   
-  ar.mod <- rep(0, N)
+  ar.mod <- numeric(N)
   ar.mod[1:AR_order] <- data[1:AR_order]
   
-  for (i in (1+AR_order):N){
-    ar.mod[i] <- sum(R[(AR_order+1):2] * data[(i-AR_order):(i-1)])
+  # Get coefficients for the AR
+  # Build the correlation matrix
+  cmat <- diag( rep( 1., AR_order + 1 ) )
+  supdiag <- function( len, ord ) cbind( 1:(len-ord), (1+ord):len )
+  subdiag <- function( len, ord ) cbind( (1+ord):len, 1:(len-ord) )
+  for( i in 1:AR_order ){
+    cmat[ supdiag( AR_order + 1, i) ] <- R[i+1]
+    cmat[ subdiag( AR_order + 1, i) ] <- R[i+1]
   }
-  
+  # The Yule-Walkerish equation for the coefficients
+  phi <- solve( cmat )%*%R[1:(AR_order+1)]
+
+  # Calculate the AR-predicted series
+  for (i in (1+AR_order):N){
+    ar.mod[i] <- sum(phi[1:AR_order] * data[(i-AR_order):(i-1)])
+  }
+  # The whitened data series.
   y.t <- data - ar.mod
   
 y.t[(AR_order+1):N]
